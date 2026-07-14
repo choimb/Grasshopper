41604f8f6d045610dd844735b78185d9fce65a7d
choimb <139409973+choimb@users.noreply.github.com>
2026-07-15 02:34:54 +0900

    Update player.js

--- js/player.js
+++ js/player.js
@@ -7,8 +7,8 @@ import { keys } from "./input.js";
 import { checkCollision } from "./collision.js";
 
 export const player = {
-    x:600,
-    y:150,
+    x:605,
+    y:170,
     width:32,
     height:32,
     sortY: 0,
