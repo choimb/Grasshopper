61d9699740b31704ed91e5f89bc20ce7f3771063
choimb <139409973+choimb@users.noreply.github.com>
2026-07-14 23:03:23 +0900

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -24,7 +24,7 @@ import {
     drawDialogue,
     openDialogue,
     nextDialogue
-} from "./dialogue.js";
+} from "./dialogue/dialogue.js"
 
 import {isKeyPressed} from "./input.js";
 import {drawMap} from "./map.js";
