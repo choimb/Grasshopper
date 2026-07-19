f99e9c20caba08c6b9b58e17268dfc5ea60b8144
choimb <139409973+choimb@users.noreply.github.com>
2026-07-19 15:32:55 +0900

    Update dongtanC.js

--- js/data/dialogue/npc/dongtanC.js
+++ js/data/dialogue/npc/dongtanC.js
@@ -9,7 +9,7 @@ export const dongtanCDialogue = {
         {
             speaker:"npc",
             emotion:"normal",
-            text:"그대에게 밀키스를 바칩니다. 저와 결혼해주시겠어요?"
+            text:"그대에게 밀키스를 바칩니다. [wait=30] 저와 결혼해주시겠어요?"
         },
         {
             speaker:"player",
